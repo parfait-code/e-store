d139b158bd0baf018be7e8310ed9503694c6ef39
parfait-code <kouamparfait5@gmail.com>
2026-07-01 07:32:25 +0200

    projet init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+      E-Store
+    </h1>
+  );
+}
